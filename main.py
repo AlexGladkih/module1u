769eca0a2f356a42bdf505f741b165cd53001849
Александр <aleksandr@MacBook-Air-Viktoria.local>
2024-06-06 21:09:00 +0800

    Initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+import fake_math as fk
+import true_math as tr
+
+result1 = fk.divide(69, 3)
+result2 = fk.divide(3, 0)
+result3 = tr.divide(49, 7)
+result4 = tr.divide(15, 0)
+print(result1)
+print(result2)
+print(result3)
+print(result4)
